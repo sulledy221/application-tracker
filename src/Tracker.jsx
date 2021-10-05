8f1284b898e6310b5782592bad9621170120ce12
Sully's Mac <sully@Sullyss-MacBook-Pro.local>
2021-10-05 16:22:04 -0700

    button configuration started

--- src/Tracker.jsx
+++ src/Tracker.jsx
@@ -1,33 +1,61 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
-export default function Tracker() {
+export default function Tracker({ addApp }) {
+  const [text, setText] = useState();
+  const handleOnClick = () => {
+    addApp({ title: text, complete: false });
+    setText("");
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       <h1>Tracker app to keep track of all your applications</h1>
-      <form>
-        <input className="input" type="text" placeholder="Company"></input>
-        <input className="input" type="text" placeholder="Position"></input>
+      <form onSubmit={onSubmit}>
         <input
+          value={text}
+          onChange={({ target: { value } }) => setText(value)}
           className="input"
-          type="text"
+          placeholder="Company"
+        ></input>
+        <input
+          value={text}
+          onChange={({ target: { value } }) => setText(value)}
+          className="input"
+          placeholder="Position"
+        ></input>
+        <input
+          value={text}
+          onChange={({ target: { value } }) => setText(value)}
+          className="input"
           placeholder="Requirements for the job"
         ></input>
         <input
+          value={text}
+          onChange={({ target: { value } }) => setText(value)}
           className="input"
-          type="text"
           placeholder="Details (maybe pros and cons)"
         ></input>
         <input
+          value={text}
+          onChange={({ target: { value } }) => setText(value)}
           className="input"
-          type="text"
           placeholder="Date you submitted the Application"
         ></input>
         <input
+          value={text}
+          onChange={({ target: { value } }) => setText(value)}
           className="input"
-          type="text"
           placeholder="Have they replied? if so enter the date here"
         ></input>
+        <button type="submit" onClick={handleOnClick}>
+          Add This Application
+        </button>
       </form>
     </div>
   );
